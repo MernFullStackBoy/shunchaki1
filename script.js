8b485331c4d8c1a6ec3017eb6eb582d33949bef5
MernFullStackBoy <jahongirmurtozayev777@gmail.com>
2024-01-08 10:49:32 +0500

    kodlar

---
+++ script.js
@@ -0,0 +1,29 @@
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", () => {
+//     console.log("You are clicked me");
+// })
+// let li = document.querySelectorAll("li")
+// li.forEach((item) => {
+//     item.addEventListener("click", (e) => {
+//         e.target.style.textDecoration = "line-through"
+//         e.target.style.opacity = '0.5'
+//     })
+// })
+// container.onmouseover = container.onmouseout = handler;
+
+// function handler(event) {
+//     function str(el) {
+//         if (!el) return "null"
+//         return el.className || el.tagName;
+//     }
+//     log.value += event.type + ": " +
+//         "target = " + str(event.target) +
+//         ", relatedTarget = " + str(event.relatedTarget) + "\n";
+//     log.scrollTop = log.scrollHeight
+//     if (event.type == "mouseover") {
+//         event.target.style.background = "pink"
+//     } if (event.type == 'mouseout') {
+//         event.target.style.background = ''
+//     }
+// }
+
